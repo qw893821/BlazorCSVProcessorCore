b0a71b503e44ec699d13f0b323675bbbb8bcd149
qw893821 <xiuman893821@gmail.com>
2021-03-23 22:33:05 -0400

    change to use input only without extra display box

---
+++ BlazorCSVProcessorCore/Models/Datarow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorCSVProcessorCore.Models
+{
+    public class Datarow
+    {
+        public List<string> DataList { get; set; }
+        //transfor row data into array of data and add to data.
+        public Datarow(string data)
+        {
+            DataList = data.Split(',').ToList();
+        }
+
+        public Datarow(List<string> data)
+        {
+            DataList = data;
+        }
+    }
+}
