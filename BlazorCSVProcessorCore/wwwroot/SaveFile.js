6d7836cf5d52444d6cf67a9d42c98b40896e0306
Wei Qin <xiuman893821@gmail.com>
2021-04-20 16:22:09 -0400

    add savefile.js content will be download, but downloaded file is object, not actual text

---
+++ BlazorCSVProcessorCore/wwwroot/SaveFile.js
@@ -0,0 +1,8 @@
+﻿function SaveFileAs(filename, content) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:text/plain;charset=uft-8," + encodeURIComponent(content);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
